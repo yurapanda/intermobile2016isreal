07bfbc0fdbb7becb8a101fc6c5ce08072890b084
yurapanda <jerrome.pelacio@ama.edu.ph>
2016-04-05 09:58:30 +0800

    changed env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,8 @@ Rails.application.configure do
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"], 
-  openssl_verify_mode: 'none'
+  password: ENV["GMAIL_PASSWORD"] 
+ 
 
   }
 end
